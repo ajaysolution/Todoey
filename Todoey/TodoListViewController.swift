9c552f1532edc40699c88c2e2f99acc8bb324315
ajaysolution <ajay.vandra.sa@gmail.com>
2020-01-27 15:29:10 +0530

    initial commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Ajay Vandra on 1/27/20.
+//  Copyright © 2020 Ajay Vandra. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["a","b","c"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
